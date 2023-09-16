ab475de3743709eea50b9226142ff380d2c3cc91
Nathan <113121626+Elli610@users.noreply.github.com>
2023-09-16 11:38:09 +0200

    fix ci

--- dist/src/addressGetter.d.ts
+++ dist/src/addressGetter.d.ts
@@ -7,4 +7,8 @@ import { Curve } from "./utils/curves";
  *
  * @returns {Promise<string[]>} The addresses from the XRPL ledger where balance >= amountMin
  */
-export declare function getAddresses(amountMin: bigint, listLength: number, curve: Curve): Promise<string[]>;
+export declare function getAddresses(
+  amountMin: bigint,
+  listLength: number,
+  curve: Curve,
+): Promise<string[]>;
--- dist/src/pubKeyGetter.d.ts
+++ dist/src/pubKeyGetter.d.ts
@@ -1,24 +1,30 @@
 import { AccountTxTransaction } from "xrpl";
 export interface Account {
-    address: string;
-    publicKey: [bigint, bigint];
-    curve: string;
+  address: string;
+  publicKey: [bigint, bigint];
+  curve: string;
 }
-export declare function getPubKeysFromAddresses(addresses: string[]): Promise<Account[]>;
+export declare function getPubKeysFromAddresses(
+  addresses: string[],
+): Promise<Account[]>;
 /**
  * Get the latest transaction from an address
  *
  * @param address - The address to get the latest transaction from
  * @returns A promise which resolves to an array of AccountTxTransaction objects
  */
-export declare function getLatestTx(address: string): Promise<AccountTxTransaction[]>;
+export declare function getLatestTx(
+  address: string,
+): Promise<AccountTxTransaction[]>;
 /**
  * Get the pubkey from the latest transaction
  *
  * @param latestTx - The latest transaction from an address
  * @returns The pubkey from the latest transaction
  */
-export declare function getXPubkeyFromLatestTx(latestTx: AccountTxTransaction[]): string;
+export declare function getXPubkeyFromLatestTx(
+  latestTx: AccountTxTransaction[],
+): string;
 /**
  * Get the XRPL address from the xPubkey
  *
