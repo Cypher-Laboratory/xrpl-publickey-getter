8ae9510da7e06c0a8574d2f7795d6f63bab4a4bb
Hussenet Thomas <Lejamon.c@protonmail.com>
2024-01-11 16:19:56 +0100

    test package

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,3 +1,7 @@
 export { getAddresses } from "./addressGetter";
 export { Curve } from "./utils/curves";
-export { getPubKeysFromAddresses, getAddressFromSigningPubkey, Account, } from "./pubKeyGetter";
+export {
+  getPubKeysFromAddresses,
+  getAddressFromSigningPubkey,
+  Account,
+} from "./pubKeyGetter";
--- dist/src/index.js
+++ dist/src/index.js
@@ -1,10 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAddressFromSigningPubkey = exports.getPubKeysFromAddresses = exports.Curve = exports.getAddresses = void 0;
+exports.getAddressFromSigningPubkey =
+  exports.getPubKeysFromAddresses =
+  exports.Curve =
+  exports.getAddresses =
+    void 0;
 var addressGetter_1 = require("./addressGetter");
-Object.defineProperty(exports, "getAddresses", { enumerable: true, get: function () { return addressGetter_1.getAddresses; } });
+Object.defineProperty(exports, "getAddresses", {
+  enumerable: true,
+  get: function () {
+    return addressGetter_1.getAddresses;
+  },
+});
 var curves_1 = require("./utils/curves");
-Object.defineProperty(exports, "Curve", { enumerable: true, get: function () { return curves_1.Curve; } });
+Object.defineProperty(exports, "Curve", {
+  enumerable: true,
+  get: function () {
+    return curves_1.Curve;
+  },
+});
 var pubKeyGetter_1 = require("./pubKeyGetter");
-Object.defineProperty(exports, "getPubKeysFromAddresses", { enumerable: true, get: function () { return pubKeyGetter_1.getPubKeysFromAddresses; } });
-Object.defineProperty(exports, "getAddressFromSigningPubkey", { enumerable: true, get: function () { return pubKeyGetter_1.getAddressFromSigningPubkey; } });
+Object.defineProperty(exports, "getPubKeysFromAddresses", {
+  enumerable: true,
+  get: function () {
+    return pubKeyGetter_1.getPubKeysFromAddresses;
+  },
+});
+Object.defineProperty(exports, "getAddressFromSigningPubkey", {
+  enumerable: true,
+  get: function () {
+    return pubKeyGetter_1.getAddressFromSigningPubkey;
+  },
+});
